7189cd6c3cd47e99e2124a6a97c6f7f8004b309f
Caryn Ligon <carynligon@gmail.com>
2016-06-24 11:51:24 -0500

    multi digit numbers working

--- main.js
+++ main.js
@@ -13,37 +13,34 @@ var operator = document.querySelectorAll('.operator');
 var equals = document.querySelector('equals');
 var clear = document.querySelector('#clear');
 var currentString = [];
+var opString = [];
+var counter = 0;
 
 // if the number loop below comes across a click, this function will run
 function getValue(evt) {
-  var enteredNumber = Number(evt.target.value);
+  var enteredNumber = String(evt.target.value);
   console.log(enteredNumber);
-  input.innerText = enteredNumber;
-  var addArray = currentString.push(enteredNumber);
-  concatenate();
-}
+  currentString[counter] = currentString + enteredNumber;
+  input.innerText = currentString;
+  }
 
-function getOperator(evt) {
-  var enteredOperator = String(evt.target.value);
-  console.log(enteredOperator);
-}
-
-// Loop for if number buttons are clicked
+// Loop for if number buttons are clicked, then go ^
 for (var i = 0; i < number.length; i++) {
   number[i].addEventListener('click', getValue);
   var value = String(number[i].value);
 }
 
+// loop for operators - maybe include these in the same loop above and then use an if, else if statements to check if values are operators or numbers, then add event listeners to the operators.
+function getOperator(evt) {
+  var enteredOperator = String(evt.target.value);
+  console.log(enteredOperator);
+  opString[counter] = opString + enteredOperator;
+  input.innerText = currentString + opString;
+}
+
 for (var i = 0; i < operator.length; i++) {
   operator[i].addEventListener('click', getOperator);
   var operator = String(operator[i].value);
-}
-
-function concatenate() {
-  var counter = currentString.length;
-  if (typeof currentString[counter] === number) {
-      console.log('hi');
-  }
 }
 
 // function calculate (evt) {
@@ -61,40 +58,3 @@ function concatenate() {
 clear.addEventListener('click', function() {
   input.innerText = '';
 });
-
-
-
-
-
-
-
-
-
-
-
-
-// WORKS:
-// var numbers = document.querySelectorAll('.number');
-// document.addEventListener('click', function(evt) {
-//   console.log(evt.target.value);
-//   if (evt.target.value === '0' || evt.target.value === '1' || evt.target.value === '2' || evt.target.value === '3' || evt.target.value === '4' || evt.target.value === '5' || evt.target.value === '6' || evt.target.value === '7' || evt.target.value === '8' || evt.target.value === '9') {
-//   document.querySelector('.result').innerText = evt.target.value;
-//   var number1 = evt.target.value;
-//   }
-// });
-
-// var zero = document.querySelector('#zero');
-// var one = document.querySelector('#one');
-// var two = document.querySelector('#two');
-// var three = document.querySelector('#three');
-// var four = document.querySelector('#four');
-// var five = document.querySelector('#five');
-// var six = document.querySelector('#six');
-// var seven = document.querySelector('#seven');
-// var eight = document.querySelector('#eight');
-// var nine = document.querySelector('#nine');
-//
-// var divide = document.querySelector('#divide');
-// var multiply = document.querySelector('#multiply');
-// var add = document.querySelector('#add');
-// var subtract = document.querySelector('#subtract');
